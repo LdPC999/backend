a94e9e7bcbef601d171641ee950df8ad9b7e6a05
Luis <luisdepablocarabe@gmail.com>
2025-04-29 10:07:19 +0200

    Configurada conexión con MongoDB Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Con esto leemos .env para la conexión con Atlas
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
