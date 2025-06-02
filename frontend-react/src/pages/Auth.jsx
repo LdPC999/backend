bb2523d7077324755eee6619f953cd820892a49f
Luis <luisdepablocarabe@gmail.com>
2025-06-02 19:26:34 +0200

    modificado .env frontend y auth para Vercel

--- frontend-react/src/pages/Auth.jsx
+++ frontend-react/src/pages/Auth.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import "../styles/Auth.css";
 import LogoAnimado from "../components/LogoAnimado"; // Importamos el logo
 
-const API_URL = "http://localhost:3000/auth";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/auth";
 
 export default function Auth() {
   const [isLogin, setIsLogin] = useState(true);
